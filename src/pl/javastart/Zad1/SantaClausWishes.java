aba2c3b5debc8c2fad8aab42d5c3c178619f104a
Alicja Swierkowska <alicja.swierkowska@gmail.com>
2017-10-24 17:30:13 +0200

    Zad nr 1

--- src/pl/javastart/Zad1/SantaClausWishes.java
+++ src/pl/javastart/Zad1/SantaClausWishes.java
@@ -20,12 +20,10 @@ public class SantaClausWishes extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
-        writer.println("Całkowita liczba znaków to: "+textStats.countSigns(textarea)+".");
-        writer.println();
-        writer.println("Całkowita liczba znaków bez spacji to: "+textStats.countSignsWoBlanks(textarea)+".");
-        writer.println();
-        writer.println("Całkowita liczba wyrazów to: "+textStats.countWords(textarea)+".");
-        writer.println();
+        writer.println("Twoje życzenia zostały wysłane do Swiętego Mikołaja. Poniżej krótka statystyka: </br>");
+        writer.println("Całkowita liczba znaków to: "+textStats.countSigns(textarea)+".</br>");
+        writer.println("Całkowita liczba znaków bez spacji to: "+textStats.countSignsWoBlanks(textarea)+".</br>");
+        writer.println("Całkowita liczba wyrazów to: "+textStats.countWords(textarea)+".</br>");
         writer.println(textStats.isPalindrome(textarea));
     }
 }
