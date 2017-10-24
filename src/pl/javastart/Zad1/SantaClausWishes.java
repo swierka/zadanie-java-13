c31ad420fe450c87e725954ee820438016875a67
Alicja Swierkowska <alicja.swierkowska@gmail.com>
2017-10-24 17:31:56 +0200

    Zad nr 1

--- src/pl/javastart/Zad1/SantaClausWishes.java
+++ src/pl/javastart/Zad1/SantaClausWishes.java
@@ -20,7 +20,9 @@ public class SantaClausWishes extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
-        writer.println("Twoje życzenia zostały wysłane do Swiętego Mikołaja. Poniżej krótka statystyka: </br>");
+        writer.println("Gratulacje! Twoje życzenia zostały wysłane do Swiętego Mikołaja!</br>");
+        writer.println("</br>");
+        writer.println("Poniżej krótka statystyka: </br>");
         writer.println("Całkowita liczba znaków to: "+textStats.countSigns(textarea)+".</br>");
         writer.println("Całkowita liczba znaków bez spacji to: "+textStats.countSignsWoBlanks(textarea)+".</br>");
         writer.println("Całkowita liczba wyrazów to: "+textStats.countWords(textarea)+".</br>");
